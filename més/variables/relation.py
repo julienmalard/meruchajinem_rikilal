49a7eee1af86207b21865ba1e4235ec8042c873b
julienmalard <julien.malard@mail.mcgill.ca>
2023-06-19 14:31:32 +0200

    Relations ordonnables

--- més/variables/relation.py
+++ més/variables/relation.py
@@ -10,3 +10,9 @@ class Relation(object):
     def __init__(soimême, indépendante: Union[Variable, GroupeVars], dépendante: Union[Variable, GroupeVars]):
         soimême.indépendante = indépendante
         soimême.dépendante = dépendante
+
+    def __lt__(soimême, autre: Relation):
+        if soimême.indépendante != autre.indépendante:
+            return str(soimême.indépendante) < str(autre.indépendante)
+        else:
+            return str(soimême.dépendante) < str(autre.dépendante)
