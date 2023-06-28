149a5e96b6d27995986e153ae16cdcf2b4d61470
julienmalard <julien.malard@mail.mcgill.ca>
2023-06-28 16:22:30 +0200

    Ajuster variables

--- més/variables/continues.py
+++ més/variables/continues.py
@@ -1,3 +1,4 @@
+from numbers import Number
 from typing import Optional, Any
 
 import pandas as pd
@@ -15,7 +16,7 @@ class VariableContinue(Variable):
             return données
         mu = soimême.générer_mu(dépendances)
 
-        ét = pm.HalfNormal(name='ét_' + soimême.nom, sigma=10)
+        ét = pm.HalfCauchy(name='ét_' + soimême.nom, beta=5)
         b = pm.Normal(
             name='b_' + soimême.nom, mu=0, sigma=100
         )
@@ -34,11 +35,42 @@ class VariablePositive(VariableContinue):
             return données
         mu = soimême.générer_mu(dépendances)
 
-        ét = pm.HalfNormal(name='ét_' + soimême.nom, sigma=10)
-        b = pm.HalfNormal(
+        ét = pm.HalfCauchy(name='ét_' + soimême.nom, beta=5)
+        b = pm.Normal(
             name='b_' + soimême.nom, sigma=100
         )
-        return pm.TruncatedNormal(name=soimême.nom, mu=mu + b, sigma=ét, lower=0, observed=données)
+        return pm.LogNormal(name=soimême.nom, mu=mu + b, sigma=ét, observed=données)
 
     def préparer_données(soimême, données: pd.Series):
-        return données / données.std()
+        return données / données.std() + 0.01
+
+
+class VariableBornée(VariableContinue):
+    def __init__(soimême, nom, minimum: Optional[Number] = None, maximum: Optional[Number] = None):
+        super().__init__(nom)
+        soimême._bornes = [minimum, maximum]
+
+    def générer_variable_pm(soimême, dépendances: dict[str, Any], données: Optional[pd.Series]):
+        if not dépendances:
+            return données
+        mu = soimême.générer_mu(dépendances)
+
+        ét = pm.HalfCauchy(name='ét_' + soimême.nom, beta=5)
+        b = pm.Normal(
+            name='b_' + soimême.nom, mu=0, sigma=100
+        )
+        return pm.LogitNormal(name=soimême.nom, mu=mu+b, sigma=ét, observed=données)
+
+    def obt_bornes(soimême, données: Optional[pd.Series]) -> list[Number, Number]:
+        minimum, maximum = soimême._bornes
+        if données is not None:
+            minimum = min(données) if minimum is None else minimum
+            maximum = max(données) if maximum is None else maximum
+        minimum = 0 if minimum is None else minimum
+        maximum = 1 if maximum is None else maximum
+        return minimum, maximum
+
+    def préparer_données(soimême, données: pd.Series):
+        bornes = soimême.obt_bornes(données)
+        données_ajustées = (données - bornes[0]) / (bornes[1] - bornes[0])
+        return données_ajustées * 0.99 + 0.005  # Pour éviter les problèmes avec le logit plus tard
